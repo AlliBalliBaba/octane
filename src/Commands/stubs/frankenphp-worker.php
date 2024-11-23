96383976c80705b5aa8bea5ae5ee5df571ac0c64
a.stecher <a.stecher@sportradar.com>
2024-11-24 00:31:18 +0100

    Checks env before server.

--- src/Commands/stubs/frankenphp-worker.php
+++ src/Commands/stubs/frankenphp-worker.php
@@ -1,7 +1,7 @@
 <?php
 
-// set a default for the app base path if it is missing
-$_SERVER['APP_BASE_PATH'] = $_SERVER['APP_BASE_PATH'] ?? $_ENV['APP_BASE_PATH'] ?? __DIR__ . '/..';
-$_SERVER['APP_PUBLIC_PATH'] = $_SERVER['APP_PUBLIC_PATH'] ?? $_ENV['APP_BASE_PATH'] ?? __DIR__;
+// set a default for the app base and public path if they are missing
+$_SERVER['APP_BASE_PATH'] = $_ENV['APP_BASE_PATH'] ?? $_SERVER['APP_BASE_PATH'] ?? __DIR__ . '/..';
+$_SERVER['APP_PUBLIC_PATH'] = $_ENV['APP_PUBLIC_PATH'] ?? $_SERVER['APP_BASE_PATH'] ?? __DIR__;
 
 require __DIR__.'/../vendor/laravel/octane/bin/frankenphp-worker.php';
