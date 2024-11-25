f03ac42cc352ac95da2f0d0f915a864dfc8c70f0
Taylor Otwell <taylor@laravel.com>
2024-11-25 15:46:25 -0600

    Update frankenphp-worker.php

--- src/Commands/stubs/frankenphp-worker.php
+++ src/Commands/stubs/frankenphp-worker.php
@@ -1,6 +1,6 @@
 <?php
 
-// set a default for the app base and public path if they are missing
+// Set a default for the application base path and public path if they are missing...
 $_SERVER['APP_BASE_PATH'] = $_ENV['APP_BASE_PATH'] ?? $_SERVER['APP_BASE_PATH'] ?? __DIR__.'/..';
 $_SERVER['APP_PUBLIC_PATH'] = $_ENV['APP_PUBLIC_PATH'] ?? $_SERVER['APP_BASE_PATH'] ?? __DIR__;
 
